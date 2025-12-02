baf392cba2950950aaa33354fe4eee2a6328114a
Peter-210 <leepeter1020@gmail.com>
2025-12-01 19:48:37 -0500

    Docker script management tool

--- Dockerfile
+++ Dockerfile
@@ -1,23 +1,8 @@
-# Use Ubuntu image as base image
-# FROM ubuntu:latest
 FROM ubuntu:noble-20251013
 
-# Install required dependencies
 RUN apt-get update && apt-get install -y \
 	g++ \
 	git \
 	cmake
 
-# (Optional) Install development dependencies
-RUN apt-get install -y \
-	gdb \
-	valgrind \
-	clang-tidy
-
-# (Optional) Install perf for profiling
-RUN apt-get install -y linux-tools-generic
-RUN rm /usr/bin/perf
-RUN ln -s /usr/lib/linux-tools/6.8.0-88-generic/perf /usr/bin/perf
-
-# Set working directory in the container
 WORKDIR /app
--- script/docker.sh
+++ script/docker.sh
@@ -1,4 +1,54 @@
 #!/bin/bash
 
-docker-compose up -d
-docker exec -it cpp-container /bin/bash
+CONTAINER_NAME=cpp-container
+
+print_help() {
+	cat <<EOF
+Usage: $0 [OPTIONS]
+
+Run and Manage Docker
+
+Options:
+	-h, --help    			  Show this help message and exit
+	-d, --dev							Create container and image for developement (uses extra dev tools)
+	-p, --prod						Create container and image for production (minimal dependencies)
+	-c, --clean						Purge docker container and image
+
+EOF
+}
+
+make_container() {
+	docker-compose --profile "$1" up -d
+	docker exec -it "$CONTAINER_NAME" /bin/bash
+}
+
+case "$1" in
+	""|-d|-dev)
+		echo "Starting docker in profile dev"
+		make_container "dev"
+		;;
+
+	-p|-prod)
+		echo "Starting docker in profile prod"
+		make_container "prod"
+		;;
+
+	-c|-clean)
+		echo "Cleaning container and image"
+		docker stop $(docker ps -aq)
+		docker rm $(docker ps -aq)
+		docker rmi -f $(docker images -q)
+		;;
+
+	-h|--help)
+		print_help
+		;;
+
+	*)
+		echo "Unknown option: $1" >&2
+		echo "Use --help for usage."
+		exit 1
+		;;
+esac
+
+exit 0
