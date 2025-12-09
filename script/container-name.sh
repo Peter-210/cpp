9b8ce0a6fb7859dcbd3e98b8b66d72e1cc4ced53
Peter-210 <leepeter1020@gmail.com>
2025-12-09 13:23:26 -0500

    minor bugs

--- script/container-name.sh
+++ script/container-name.sh
@@ -15,10 +15,13 @@ then
 	exit 1
 fi
 
+CURR_FILE_PATH="$0"
+CURR_DIR_PATH="${CURR_FILE_PATH%/*}"
+
+cd $CURR_DIR_PATH
 sed -i "s/CONTAINER_NAME=$OLD_NAME/CONTAINER_NAME=$NEW_NAME/g" "docker.sh"
 sed -i "s/container_name: $OLD_NAME/container_name: $NEW_NAME/g" "../docker-compose.yml"
 
 echo "Docker container name has changed to '$NEW_NAME'"
 
-# Delete this script file
-rm -- "$0"
+rm -- $CURR_FILE_PATH
