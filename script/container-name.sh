36cf018d7dfe9de18f55e7d400fb3cb074b3a97e
Peter-210 <leepeter1020@gmail.com>
2025-12-09 13:12:28 -0500

    enable script actions

--- script/container-name.sh
+++ script/container-name.sh
@@ -15,10 +15,10 @@ then
 	exit 1
 fi
 
-#sed -i "s/CONTAINER_NAME=$OLD_NAME/CONTAINER_NAME=$NEW_NAME/g" "docker.sh"
-#sed -i "s/container_name: $OLD_NAME/container_name: $NEW_NAME/g" "../docker-compose.yml"
+sed -i "s/CONTAINER_NAME=$OLD_NAME/CONTAINER_NAME=$NEW_NAME/g" "docker.sh"
+sed -i "s/container_name: $OLD_NAME/container_name: $NEW_NAME/g" "../docker-compose.yml"
 
 echo "Docker container name has changed to '$NEW_NAME'"
 
 # Delete this script file
-#rm -- "$0"
+rm -- "$0"
