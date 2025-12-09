e3323189535accb70f11471597f33fef2107472a
Peter-210 <leepeter1020@gmail.com>
2025-12-09 13:07:28 -0500

    Modify to be cleaner

--- script/container-name.sh
+++ script/container-name.sh
@@ -2,13 +2,23 @@
 
 OLD_NAME="cpp-container"
 
-echo "Enter a unique docker container name: "
-read NEW_NAME
+read -p "Enter new container name: " NEW_NAME
+echo
 
-sed -i "s/CONTAINER_NAME=$OLD_NAME/CONTAINER_NAME=$NEW_NAME/g" "docker.sh"
-sed -i "s/container_name: $OLD_NAME/container_name: $NEW_NAME/g" "../docker-compose.yml"
+echo "This will change project files to use '$NEW_NAME' container name"
+read -p "Are you sure? (Press 'y' to proceed) " -n 1 -r USER_REPLY
+echo
+
+if [[ ! $USER_REPLY =~ ^[Yy]$ ]]
+then
+	echo "Terminated action to change container name"
+	exit 1
+fi
+
+#sed -i "s/CONTAINER_NAME=$OLD_NAME/CONTAINER_NAME=$NEW_NAME/g" "docker.sh"
+#sed -i "s/container_name: $OLD_NAME/container_name: $NEW_NAME/g" "../docker-compose.yml"
 
 echo "Docker container name has changed to '$NEW_NAME'"
 
 # Delete this script file
-# rm -- "$0"
+#rm -- "$0"
