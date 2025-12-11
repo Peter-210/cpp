c12b2bcd53deefbfacd4ac084de2cbe31be0b190
Peter-210 <leepeter1020@gmail.com>
2025-12-11 11:26:05 -0500

    finish setup script

--- script/setup.sh
+++ script/setup.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+# cd to project directory
 CURR_FILE_PATH="$0"
 CURR_DIR_PATH="${CURR_FILE_PATH%/*}"
-
 cd $CURR_DIR_PATH
-sed -i "s/CONTAINER_NAME=$OLD_NAME/CONTAINER_NAME=$NEW_NAME/g" "docker.sh"
-sed -i "s/container_name: $OLD_NAME/container_name: $NEW_NAME/g" "../docker-compose.yml"
+cd ..
+
+# Load setup variables
+source public.env
+
+# Change file variables to match setup variables
+sed -i "s/^\(CONTAINER_NAME=\).*/\1$CONTAINER_NAME/g" "script/docker.sh"
+sed -i "s/^\(container_name: \).*/\1$CONTAINER_NAME/g" "docker-compose.yml"
+
+# sed -i "s/CONTAINER_NAME=$OLD_NAME/CONTAINER_NAME=$CONTAINER_NAME/g" "script/docker.sh"
+# sed -i "s/container_name: $OLD_NAME/container_name: $CONTAINER_NAME/g" "docker-compose.yml"
+
+echo "Setup complete"
