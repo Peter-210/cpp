f7ba80614cc54e775f89294456debc223384b620
Peter-210 <leepeter1020@gmail.com>
2025-12-11 20:32:52 -0500

    correct image branching

--- script/setup.sh
+++ script/setup.sh
@@ -24,6 +24,7 @@ sed -i "s/\(container_name: \).*/\1$CONTAINER_NAME/g" "docker-compose.yml"
 sed -i "s/\(CONTAINER_NAME=\).*/\1$CONTAINER_NAME/g" "script/docker.sh"
 
 sed -i "2s/.*/  ${IMAGE_NAME}:/" "docker-compose.yml"
+sed -i "s/extends:.*/extends: ${IMAGE_NAME}/" "docker-compose.yml"
 sed -i "s/\(IMAGE_PROD_NAME=\).*/\1${IMAGE_NAME}-prod/g" "script/docker.sh"
 sed -i "s/\(IMAGE_DEV_NAME=\).*/\1${IMAGE_NAME}-dev/g" "script/docker.sh"
 
