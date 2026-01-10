cf96ca410a7b202e31ee76a3b92c3470cbf453b0
Peter-210 <leepeter1020@gmail.com>
2026-01-09 19:49:33 -0500

    removed deprecated feature

--- src/myModule.cpp
+++ src/myModule.cpp
@@ -18,13 +18,6 @@
  * but here's a description.
  *
  * @return The string "Hello World".
- *
- * @details
- * This detailed description explains that this function 
- * returns the string "Hello World" when called.
- *
- * @usage
- * std::string myHello = hello(); // myHello holds the value "Hello World"
  */
 std::string hello()
 {
